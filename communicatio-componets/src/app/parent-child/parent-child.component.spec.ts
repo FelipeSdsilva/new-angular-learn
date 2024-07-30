26192df9d54e5a6ea6e3eba6a99866f43319cce0
Felipe Sousa <felipe.fps09@hotmail.com>
2024-07-30 10:52:35 -0300

    Add parent-child component

---
+++ communicatio-componets/src/app/parent-child/parent-child.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ParentChildComponent } from './parent-child.component';
+
+describe('ParentChildComponent', () => {
+  let component: ParentChildComponent;
+  let fixture: ComponentFixture<ParentChildComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ParentChildComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ParentChildComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ communicatio-componets/src/app/parent-child/parent-child.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parent-child',
+  standalone: true,
+  imports: [],
+  templateUrl: './parent-child.component.html',
+  styleUrl: './parent-child.component.css'
+})
+export class ParentChildComponent {
+
+}
